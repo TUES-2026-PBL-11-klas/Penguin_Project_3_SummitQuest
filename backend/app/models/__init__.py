863e13c3a4372b90941b8b820a3a1866195c831c
Maycheto <maya.g.mircheva@gmail.com>
2026-05-31 16:56:46 +0300

    models __init__ re-exports

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,7 @@
-from .user import User
-from .quest import Quest
-from .trail_point import TrailPoint
-from .email_verification import EmailVerification
-from .quest_stats import QuestStats
-from .badge import Badge
-from .user_badge import UserBadge
+from .user import User as User
+from .quest import Quest as Quest
+from .trail_point import TrailPoint as TrailPoint
+from .email_verification import EmailVerification as EmailVerification
+from .quest_stats import QuestStats as QuestStats
+from .badge import Badge as Badge
+from .user_badge import UserBadge as UserBadge
